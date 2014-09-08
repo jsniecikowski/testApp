2c1b7d8ba92f2c22a4d9e00efa777b53ca86de34
Jacek Sniecikowski <jacek@sniecikowski.com>
2014-09-07 19:27:10 -0700

    Renaming main class

--- src/com/jacek/simpleApp/Hello.java
+++ src/com/jacek/simpleApp/Hello.java
@@ -2,7 +2,7 @@ package com.jacek.simpleApp;
 
 import sun.misc.MessageUtils;
 
-public class Main {
+public class Hello {
 
     public static void main(String[] args) {
 	MessageUtils.toStdout("Hello");
